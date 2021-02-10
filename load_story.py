07b29fcede663d1043cd6ef3cf425dcfcefd2bf3
wolfgangvanbach <wolfgangvanbachtest@gmail.com>
2021-02-10 02:52:17 +0000

    TheAdventuresofBreadBoyUpdate...

---
+++ load_story.py
@@ -0,0 +1,16 @@
+import pickle
+
+story = {
+	1: {
+	  'Text': [ 
+	   "*ALARM BEEP*ALARM BEEP*ALARM BEEP*",
+	   "Bread Boy wake up! YO BREAD BOY!",
+	   "Lets go Bread Boy the universe awaits..."
+	],
+	  'Options': [
+	      ("KAMEHAMEHA MOFOS ITS BREAD BOY!", 2),
+	      ("WUBBA LUBBA DUB DUB What a night!", 3)
+	]
+	},
+	2: {
+	  '
